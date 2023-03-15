407e8e66815a4deda5be0332529d7292008a33c5
wil1i <williams.ca001@gmail.com>
2023-03-15 13:05:24 +0330

    Auto refresh enabled, style update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,12 +3,17 @@ const DutyInformation = require("../models/DutyInformation")
 const User = require("../models/User")
 
 const get = async (req, res) => {
-    const users = await User.findAll()
+    const users = await User.findAll({order : [['dutyHours', 'ASC'], ['dutyMinutes', 'ASC']]})
+    const duties = await Duty.findAll()
+    let dutyList = []
+
+    duties.forEach(duty => { dutyList.push(duty.codePersoneli) })
 
     res.render("admin", {
         user : req.user,
         flash : req.flash(),
-        users
+        users,
+        dutyList
     })
 }
 
