669ff5bf01b375b6f5bba704597270437de91426
Wil1i <williams.ca001@gmail.com>
2023-04-17 19:28:52 +0430

    Mobile system completed

--- controllers/api/dutyController.js
+++ controllers/api/dutyController.js
@@ -5,19 +5,8 @@ const post = async (req, res) => {
   const isUserDuty = await Duty.findOne({
     where: { codePersoneli: req.body.codePersoneli },
   });
-  if (isUserDuty) {
-    axios
-      .post("/?action=logout", { codePersoneli: req.body.codePersoneli })
-      .then(() => {
-        res.send(true);
-      });
-  } else {
-    axios
-      .post("/?action=login", { codePersoneli: req.body.codePersoneli })
-      .then(() => {
-        res.send(true);
-      });
-  }
+
+  isUserDuty ? res.send(true) : res.send(false);
 };
 
 module.exports = {
